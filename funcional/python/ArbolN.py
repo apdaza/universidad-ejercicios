13411f060d045eab4df327227572812c8d9a1009
apdaza <apdaza@gmail.com>
2020-06-30 13:39:01 -0500

    modificado arboles narios

--- funcional/python/ArbolN.py
+++ funcional/python/ArbolN.py
@@ -16,13 +16,39 @@ def buscar_en_hijos(lista, valor):
 		return False
 	else:
 		return buscar(lista[0], valor) or buscar_en_hijos(lista[1:], valor)
+
+def ver_arbol(arbol):
+	if arbol == None:
+		return []
+	else:
+		return [arbol.valor] + ver_hijos(arbol.hijos)
+
+def ver_hijos(lista):
+	if lista==[]:
+		return []
+	else:
+		return [ver_arbol(lista[0])] + ver_hijos(lista[1:])
+
+def insertar(arbol, valor):
+	if arbol == None:
+		return NodoN(valor)
+	if arbol.valor == valor[:-1]:
+		return NodoN(arbol.valor, arbol.hijos+[NodoN(valor)])
+	return insertar_hijos(arbol.hijos,valor)
+
+def insertar_hijos(lista, valor):
+	if lista == []:
+		return None
+	return insertar(lista[0], valor); insertar_hijos(lista[1:], valor)
 	
 
-arbol = NodoN(10,[NodoN(20,[NodoN(40),
-							NodoN(44)]),
-				  NodoN(50,[NodoN(150)]),
-				  NodoN(100),
-				  NodoN(1000,[NodoN(2000),NodoN(3000),NodoN(5000)])])
+arbol = NodoN('1',[NodoN('12',[NodoN('121'),
+							NodoN('122')]),
+				  NodoN('15',[NodoN('151')]),
+				  NodoN('10'),
+				  NodoN('13',[NodoN('131'),NodoN('132'),NodoN('133')])])
 
-print buscar(arbol, 20)
-print buscar(arbol, 3000)
+#print(buscar(arbol, '20'))
+#print(buscar(arbol, '130'))
+print(ver_arbol(arbol))
+print(ver_arbol((insertar(arbol, '120'))))
