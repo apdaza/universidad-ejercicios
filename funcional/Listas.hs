10134bc4b2e95f1475edc91f7935fb4c1fb8f264
apdaza <apdaza@gmail.com>
2021-04-27 13:17:40 -0500

    actualizados ejemplos de funcional

--- funcional/Listas.hs
+++ funcional/Listas.hs
@@ -3,11 +3,14 @@
 -}
 
 module Listas where
+cuadrado::Int->Int 
+cuadrado n = n*n
+
 doblarLista::[Int]
 doblarLista = [2*x | x <- [0..10]]
 
 doblarLista2::[Int]->[Int]
-doblarLista2 (x:xs) = [2*x | x <- (x:xs)]
+doblarLista2 l = [2*x | x <- l]
 
 sumaLista::[Int]->Int
 sumaLista [] = 0
@@ -16,3 +19,14 @@ sumaLista(x:xs) = x + sumaLista xs
 ultimo_elemento::[Int]->Int
 ultimo_elemento [x] = x
 ultimo_elemento (x:xs) = ultimo_elemento xs
+
+cuadradoLista::[Int]->[Int]
+cuadradoLista [] = []
+cuadradoLista lista = [x*x | x <- lista]
+
+cuadradoLista2::[Int]->[Int]
+cuadradoLista2 [] = []
+cuadradoLista2 (y:ys) = y*y : cuadradoLista2 ys
+
+cuadradoLista3::[Int]->[Int]
+cuadradoLista3 lista = map cuadrado lista
--- funcional/Matematicas.hs
+++ funcional/Matematicas.hs
@@ -1,7 +1,6 @@
 {-
   modulo que implementa funciones matematicas simples
-  programacion funcional
-
+  en programacion funcional
 -}
 
 module Matematicas where
@@ -24,7 +23,7 @@ producto a b = a * b
 --divide dos flotantes
 division::Float->Float->Float
 division a b = if b == 0 then 0.0
-	       else a/b
+               else a/b
 
 --factorial de un numero
 factorial::Int->Int
@@ -40,9 +39,10 @@ fibonacci n = fibonacci(n-1)+fibonacci(n-2)
 
 --calcula la potencia de a a la b
 potencia::Int->Int->Int
-potencia a b = if b == 0 then 1
-	       else if a == 1 then 1
-	       else a*potencia a (b-1)
+potencia a b
+  | b == 0 = 1
+  | a == 1 = 1
+  | otherwise = a*potencia a (b-1)
 
 --potencia con pseudonimos
 potenciaSeudo::Int->Int->Int
@@ -50,26 +50,31 @@ potenciaSeudo a 0 = 1
 potenciaSeudo 1 b = 1
 potenciaSeudo a b = a*potenciaSeudo a (b-1)
 
+potenciaCondicional::Int->Int->Int
+potenciaCondicional a b = if b == 0 then 1
+						  else if a == 1 then 1
+						  else a * potenciaCondicional a (b-1)
+
 --suma los digitos de un numero
 sumaDigitos::Int->Int
-sumaDigitos n = if n < 10 then n 
-	    else sumaDigitos(div n 10) + mod n 10
-	    
+sumaDigitos n = if n < 10 then n
+            else sumaDigitos(div n 10) + mod n 10
+
 --suma los digitos de un numero usando guardas
 sumaDigitosGuardas::Int->Int
-sumaDigitosGuardas n 
-	| n < 10 = n 
-	| otherwise = sumaDigitosGuardas(div n 10) + mod n 10
+sumaDigitosGuardas n
+        | n < 10 = n
+        | otherwise = sumaDigitosGuardas(div n 10) + mod n 10
 
 --convierte de decimal a binario
 diezAdos::Int->Int
 diezAdos n = if n<2 then n
-	     else mod n 2 + diezAdos(div n 2) * 10
+             else mod n 2 + diezAdos(div n 2) * 10
 
 --convierte de binario a decimal
 dosAdiez::Int->Int
 dosAdiez n = if n<2 then n
-	     else mod n 10 + dosAdiez(div n 10) * 2
+             else mod n 10 + dosAdiez(div n 10) * 2
 
 --genera el siguiente numero del parametros de entrada hasta 9 reiniciando en 1
 siguiente::Int->Int
@@ -80,3 +85,16 @@ siguiente n = if n < 9 then n+1
 anterior::Int->Int
 anterior n = if n < 1 || n > 9 then 9
              else n-1
+
+--expresión que permite multiplicar sin multiplicar
+sinMultiplicar::Int->Int->Int
+sinMultiplicar a b 
+	| b == 0 = 0
+	| b == 1 = a
+	| otherwise = a + sinMultiplicar a (b-1)
+
+-- expresión que permite dividir sin dividir
+sinDividir::Int->Int->Int 
+sinDividir a b
+	| b > a = 0
+	| otherwise = 1 + sinDividir (a-b) b
